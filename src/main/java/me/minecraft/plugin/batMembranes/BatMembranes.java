10ace664b41886ccda40cfeae2a5fde2fda58ab8
justeriksk <sebo.erik@gmail.com>
2025-06-08 18:16:27 +0200

    added a config file

--- src/main/java/me/minecraft/plugin/batMembranes/BatMembranes.java
+++ src/main/java/me/minecraft/plugin/batMembranes/BatMembranes.java
@@ -3,6 +3,7 @@ package me.minecraft.plugin.batMembranes;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -21,6 +22,13 @@ public final class BatMembranes extends JavaPlugin implements Listener {
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "BatMembranes >> Plugin has been enabled!");
         this.getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+
+        FileConfiguration config = this.getConfig();
+        config.addDefault("membrane_drop_percentage", 25);
+        config.addDefault("membrane_drop_amount", 1);
+        config.addDefault("membrane_bonus_drop_percentage", 15);
+        config.addDefault("membrane_bonus_drop_amount", 2);
     }
 
     @EventHandler
@@ -28,12 +36,32 @@ public final class BatMembranes extends JavaPlugin implements Listener {
         LivingEntity ent = e.getEntity();
         Random ran = new Random();
         int num = ran.nextInt(100);
+        int num_bon = ran.nextInt(100);
+
+        int mem_drop_per = this.getConfig().getInt("membrane_drop_percentage", 25);
+        int mem_drop_am = this.getConfig().getInt("membrane_drop_amount", 1);
+        int mem_bon_drop_per = this.getConfig().getInt("membrane_bonus_drop_percentage", 15);
+        int mem_bon_drop_am = this.getConfig().getInt("membrane_bonus_drop_amount", 2);
+
+        if (mem_drop_per > 100 || mem_drop_per < 1) {
+            mem_drop_per = 25;
+        }
+        if (mem_drop_am > 10 || mem_drop_am < 1) {
+            mem_drop_am = 1;
+        }
+        if (mem_bon_drop_per > 30 || mem_bon_drop_per < 1) {
+            mem_bon_drop_per = 15;
+        }
+        if (mem_bon_drop_am > 3 || mem_bon_drop_am < 1) {
+            mem_bon_drop_am = 2;
+        }
 
         if (ent.getType() == EntityType.BAT) {
-            if (num < 25) {
-                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE));
-            } else if (num > 85) {
-                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, 2));
+            if (num <= mem_drop_per) {
+                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, mem_drop_am));
+            }
+            if (num_bon <= mem_bon_drop_per) {
+                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, mem_bon_drop_am));
             }
         }
     }
