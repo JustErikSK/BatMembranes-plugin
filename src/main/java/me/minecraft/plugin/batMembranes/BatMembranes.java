4e56ba6d104b39c0fb4a1db65985d8d15a7faac9
justerik <sebo.erik@gmail.com>
2025-10-08 22:47:01 +0200

    updated to 1.21.10, added option to turn on phantoms, added option to turn off membrane bonus drops, increased max percentage for bonus drop to 50, increased max amount for bonus drop to 5, renamed phantom membranes to bat membranes

--- src/main/java/me/minecraft/plugin/batMembranes/BatMembranes.java
+++ src/main/java/me/minecraft/plugin/batMembranes/BatMembranes.java
@@ -12,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
@@ -20,15 +21,23 @@ public final class BatMembranes extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "BatMembranes >> Plugin has been enabled!");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "Bat Membranes >> Plugin has been enabled!");
         this.getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
+
+        getLogger().info("Bat Membranes is loading drop percentages and amounts...");
+        getLogger().info("Checking for bonus drops...");
+        getLogger().info("Checking if you want phantoms...");
 
         FileConfiguration config = this.getConfig();
         config.addDefault("membrane_drop_percentage", 25);
         config.addDefault("membrane_drop_amount", 1);
         config.addDefault("membrane_bonus_drop_percentage", 15);
         config.addDefault("membrane_bonus_drop_amount", 2);
+        config.addDefault("membrane_bonus_drop", true);
+        config.addDefault("phantoms", false);
+
+        getLogger().info("Config loaded!");
     }
 
     @EventHandler
@@ -49,19 +58,33 @@ public final class BatMembranes extends JavaPlugin implements Listener {
         if (mem_drop_am > 10 || mem_drop_am < 1) {
             mem_drop_am = 1;
         }
-        if (mem_bon_drop_per > 30 || mem_bon_drop_per < 1) {
+        if (mem_bon_drop_per > 50 || mem_bon_drop_per < 1) {
             mem_bon_drop_per = 15;
         }
-        if (mem_bon_drop_am > 3 || mem_bon_drop_am < 1) {
+        if (mem_bon_drop_am > 5 || mem_bon_drop_am < 1) {
             mem_bon_drop_am = 2;
         }
 
+        ItemStack phantom_membrane1 = new ItemStack(Material.PHANTOM_MEMBRANE, mem_drop_am);
+        ItemMeta itemStackMeta1 = phantom_membrane1.getItemMeta();
+        itemStackMeta1.setDisplayName("Bat Membrane");
+        phantom_membrane1.setItemMeta(itemStackMeta1);
+
+        ItemStack phantom_membrane2 = new ItemStack(Material.PHANTOM_MEMBRANE, mem_bon_drop_am);
+        ItemMeta itemStackMeta2 = phantom_membrane2.getItemMeta();
+        itemStackMeta2.setDisplayName("Bat Membrane");
+        phantom_membrane2.setItemMeta(itemStackMeta2);
+
+        boolean allowBonusDrop = this.getConfig().getBoolean("membrane_bonus_drop", true);
+
         if (ent.getType() == EntityType.BAT) {
             if (num <= mem_drop_per) {
-                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, mem_drop_am));
+                e.getDrops().add(phantom_membrane1);
             }
-            if (num_bon <= mem_bon_drop_per) {
-                e.getDrops().add(new ItemStack(Material.PHANTOM_MEMBRANE, mem_bon_drop_am));
+            if (allowBonusDrop) {
+                if (num_bon <= mem_bon_drop_per) {
+                    e.getDrops().add(phantom_membrane2);
+                }
             }
         }
     }
@@ -70,8 +93,12 @@ public final class BatMembranes extends JavaPlugin implements Listener {
     public void phantomSpawn(EntitySpawnEvent e) {
         Entity ent = e.getEntity();
 
-        if (ent.getType() == EntityType.PHANTOM) {
-            e.setCancelled(true);
+        boolean allowPhantoms = this.getConfig().getBoolean("phantoms", false);
+
+        if (!allowPhantoms) {
+            if (ent.getType() == EntityType.PHANTOM) {
+                e.setCancelled(true);
+            }
         }
     }
 }
